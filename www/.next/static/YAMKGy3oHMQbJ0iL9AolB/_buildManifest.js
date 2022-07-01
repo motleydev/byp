336e9de759d4e17d8864e2fdbe6df88b4e5071f7
Jesse Martin <themotleydev@gmail.com>
2022-07-01 16:51:53 +0200

    vercel changes

---
+++ www/.next/static/YAMKGy3oHMQbJ0iL9AolB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-f715c2133d9098e1.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
